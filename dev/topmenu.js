6294f434b9cf068bf2841bccdf9883eb5326b9c1
Paul Keutelian <paulkeutelian@gmail.com>
2016-06-04 20:51:12 -0700

    Converted menu to be a common javascript file so there's only one file to edit. Issues:#10
    Signed-off-by: Paul Keutelian <paulkeutelian@gmail.com>

---
+++ dev/topmenu.js
@@ -0,0 +1,10 @@
+document.write('\
+<div class="topmenu">\
+	<a href="index.html" style="font-weight:700">Paul</a>\
+	<a href="blog.html">Blog</a>\
+	<a href="projects.html">Projects</a>\
+	<a href="resources.html">Resources</a>\
+	<a href="resume.html">Resume</a>\
+	<a href="contact.html">Contact</a>\
+</div>\
+');
